9dee7c3ca208d81edffe3d2ca25797b0657f2485
ripperi <22086553+ripperi@users.noreply.github.com>
2017-02-20 21:17:09 +0200

    Refactor classes

--- src/cljs/clj_chat/views.cljs
+++ src/cljs/clj_chat/views.cljs
@@ -3,11 +3,11 @@
             [reagent.core :refer [atom]]))
 
 (defn message-view [message]
-  [:li {:class "message" :key (:time message)} (:value message)])
+  [:li.message {:key (:time message)} (:value message)])
 
 (defn messages-view []
   (let [messages (re-frame/subscribe [:messages])]
-    [:ul {:class "messages"}
+    [:ul.messages
      (if (not (empty? @messages))
        (map message-view @messages))]))
 
@@ -19,23 +19,23 @@
                                  (re-frame/dispatch [:send-message {:time (str (.getTime (js/Date.))) :value @value}])
                                  (reset! value "")))]
     (fn []
-      [:div {:class "content flex-col"}
+      [:div.content.flex-col
        (messages-view)
-       [:div {:class "text-wrap"}
-        [:form {:class "text-wrap-inner" :on-submit submit-handler}
-         [:input {:class "text-area" :on-change change-handler :placeholder "Message..." :value @value}]
+       [:div.text-wrap
+        [:form.text-wrap-inner {:on-submit submit-handler}
+         [:input.text-area{:on-change change-handler :placeholder "Message..." :value @value}]
          [:input {:type "submit"}]]]])))
 
 (defn groups-view []
   [:div {:class "groups"}])
 
 (defn group-view []
-  [:div {:class "group"}
-   [:div {:class "edge-wrap"}]
+  [:div.group
+   [:div.edge-wrap]
    [content-view]
-   [:div {:class "edge-wrap"}]])
+   [:div.edge-wrap]])
 
 (defn main-panel []
-  [:div {:class "main flex-row overflow-hidden"}
+  [:div.main.flex-row.overflow-hidden
    (groups-view)
    (group-view)])
