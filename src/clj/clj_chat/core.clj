0b9f75fbc99ce58a833169829e51df75334a5928
ripperi <22086553+ripperi@users.noreply.github.com>
2017-03-17 18:25:54 +0200

    Move logic to a separate file

--- src/clj/clj_chat/core.clj
+++ src/clj/clj_chat/core.clj
@@ -1,1 +1,70 @@
 (ns clj-chat.core)
+
+;; ----------atoms to keep track of rooms and users----------
+
+(defonce rooms_ (atom {}))
+
+(defonce users_ (atom {}))
+
+;; ----------functions for managing said atoms----------
+
+(defn add-room!
+  ([name]
+   (add-room! name nil))
+  ([name creator-id]
+   (swap! rooms_ assoc (keyword name) {:id name :name name :users {} :channels ["#general"] :owner creator-id})))
+
+(defn add-channel! [room channel]
+  (swap! rooms_ update-in [(keyword room) :channels] conj channel))
+
+(defn add-user!
+  ([id]
+   (add-user! id nil []))
+  ([id name room-keys]
+   (swap! users_ assoc (keyword id) {:id id :name name :rooms room-keys})))
+
+(defn add-to-room! [room user]
+  (swap! rooms_ update-in [(keyword room) :users] assoc (keyword user) {:id user :name nil})
+  (swap! users_ update-in [(keyword user) :rooms] conj room))
+
+(defn remove-from-room! [room user]
+  (swap! rooms_ update-in [(keyword room) :users] dissoc (keyword user))
+  (swap! users_ update-in [(keyword user) :rooms] #(vec (remove #{room} %))))
+
+(defn remove-user! [user]
+  (doseq [room (:rooms ((keyword user) @users_))]
+    (remove-from-room! room user))
+  (swap! users_ dissoc (keyword user)))
+
+(defn update-username-for-rooms! [user username]
+  (let [users-rooms (get-in @users_ [(keyword user) :rooms])]
+    (doseq [room users-rooms]
+      (swap! rooms_ assoc-in [(keyword room) :users (keyword user) :name] username))))
+
+(defn set-username! [user username]
+  (swap! users_ assoc-in [(keyword user) :name] username)
+  (update-username-for-rooms! user username))
+
+(defn get-rooms-with-keys [user]
+  (select-keys @rooms_ (map keyword (get-in @users_ [(keyword user) :rooms]))))
+
+(defn get-rooms [user]
+  (map #((keyword %) @rooms_) (:rooms ((keyword user) @users_))))
+
+(defn get-users-in-room [room]
+  (map :id (vals (get-in @rooms_ [(keyword room) :users]))))
+
+(defn login-needed? [user]
+  (nil? (:name ((keyword user) @users_))))
+
+(defn valid-message? [msg]
+  (not (or (nil? (:channel msg)) (nil? (:group msg)))))
+
+(defn allowed-message? [uuid msg]
+  (let [user ((keyword uuid) @users_)
+        users-rooms (:rooms user)
+        msg-room (:group msg)
+        msg-channel (:channel msg)
+        room-channels (:channels ((keyword msg-room) @rooms_))]
+    (and (some #(= msg-room %) users-rooms)
+         (some #(= msg-channel %) room-channels))))
--- src/clj/clj_chat/handler.clj
+++ src/clj/clj_chat/handler.clj
@@ -1,5 +1,6 @@
 (ns clj-chat.handler
-  (:require [compojure.core :refer [GET POST defroutes]]
+  (:require [clj-chat.core :as core]
+            [compojure.core :refer [GET POST defroutes]]
             [compojure.route :refer [resources]]
             [ring.util.response :refer [resource-response content-type]]
             [ring.middleware.reload :refer [wrap-reload]]
@@ -36,84 +37,14 @@
   (resources "/"))
 
 (def ring-handler
-
-;; ----------atoms to keep track of rooms and users----------
-
-(defonce rooms_ (atom {}))
-
-(defonce users_ (atom {}))
-
-;; ----------functions for managing said atoms----------
-
-(defn add-room
-  ([rooms name]
-   (add-room rooms name nil))
-  ([rooms name creator-id]
-   (assoc rooms
-    (keyword name) {:id name :name name :users {} :channels ["#general"] :owner creator-id})))
-
-(defn add-user
-  ([users id]
-   (add-user users id nil []))
-  ([users id name room-keys]
-   (assoc users (keyword id) {:id id :name name :rooms room-keys})))
-
-(defn add-channel! [rooms room channel]
-  (swap! rooms update-in [(keyword room) :channels] conj channel))
-
-(defn add-to-room! [rooms users room user]
-  (swap! rooms update-in [(keyword room) :users] assoc (keyword user) {:id user :name nil})
-  (swap! users update-in [(keyword user) :rooms] conj room))
-
-(defn remove-from-room! [rooms users room user]
-  (swap! rooms update-in [(keyword room) :users] dissoc (keyword user))
-  (swap! users update-in [(keyword user) :rooms] #(vec (remove #{room} %))))
-
-(defn remove-user! [rooms users user]
-  (doseq [room (:rooms ((keyword user) @users))]
-    (remove-from-room! rooms users room user))
-  (swap! users dissoc (keyword user)))
-
-(defn get-rooms-with-keys [rooms users user]
-  (select-keys rooms (map keyword (get-in users [(keyword user) :rooms]))))
-
-(defn get-rooms [rooms users user]
-  (map #((keyword %) rooms) (:rooms ((keyword user) users))))
-
-(defn get-users-in-room [rooms room]
-  (map :id (vals (get-in rooms [(keyword room) :users]))))
-
-(defn login-needed? [users user]
-  (nil? (:name ((keyword user) users))))
-
-(defn valid-message? [msg]
-  (not (or (nil? (:channel msg)) (nil? (:group msg)))))
-
-(defn allowed-message? [uuid msg]
-  (let [user ((keyword uuid) @users_)
-        users-rooms (:rooms user)
-        msg-room (:group msg)
-        msg-channel (:channel msg)
-        room-channels (:channels ((keyword msg-room) @rooms_))]
-    (and (some #(= msg-room %) users-rooms)
-         (some #(= msg-channel %) room-channels))))
-
-(defn update-username-for-rooms! [rooms users user username]
-  (let [users-rooms (get-in @users_ [(keyword user) :rooms])]
-    (doseq [room users-rooms]
-      (swap! rooms assoc-in [(keyword room) :users (keyword user) :name] username))))
-
-(defn set-username! [rooms users user username]
-  (swap! users assoc-in [(keyword user) :name] username)
-  (update-username-for-rooms! rooms users user username))
   (-> #'ring-routes
       (cond-> (env :port) wrap-reload)
       (ring.middleware.defaults/wrap-defaults ring.middleware.defaults/site-defaults)))
 
 ;; ----------sente send events----------
 
-(defn update-clients-rooms [rooms users user]
-  (chsk-send! user [:update/rooms (get-rooms rooms users user)]))
+(defn update-clients-rooms [user]
+  (chsk-send! user [:update/rooms (core/get-rooms user)]))
 
 (defn send-login-need-status [uid bool]
   (chsk-send! uid [:update/login-need bool]))
@@ -137,57 +68,57 @@
 (defmethod -event-msg-handler
   :chsk/uidport-close
   [{:keys [uid]}]
-  (remove-user! rooms_ users_ uid)
-  (println (str "\nuidport-close\n" @users_ "\n\n" @rooms_ "\n")))
+  (core/remove-user! uid)
+  (println (str "\nuidport-close\n" @core/users_ "\n\n" @core/rooms_ "\n")))
 
 (defmethod -event-msg-handler
   :chsk/uidport-open
   [{:keys [uid]}]
-  (reset! users_ (add-user @users_ uid))
-  (println (str "\nuidport-open\n" @users_ "\n\n" @rooms_ "\n")))
+  (core/add-user! uid)
+  (println (str "\nuidport-open\n" @core/users_ "\n\n" @core/rooms_ "\n")))
 
 ;; -----
 
 (defmethod -event-msg-handler
   :clj-chat.events/message
   [{:keys [uid ?data]}]
-  (if (and (valid-message? ?data) (allowed-message? uid ?data))
-    (doseq [uuid (get-users-in-room @rooms_ (:group ?data))]
+  (if (and (core/valid-message? ?data) (core/allowed-message? uid ?data))
+    (doseq [uuid (core/get-users-in-room (:group ?data))]
       (chsk-send! uuid [::message ?data]))))
 
 (defmethod -event-msg-handler
   :room/add
   [{:keys [uid ?data]}]
-  (if-not (map? ((keyword ?data) @rooms_))
-    (do (reset! rooms_ (add-room @rooms_ ?data uid))
-        (add-to-room! rooms_ users_ ?data uid)))
-  (update-clients-rooms @rooms_ @users_ uid)
-  (println (str "\nadd room\n" @rooms_ "\n" @users_ "\n")))
+  (if-not (map? ((keyword ?data) @core/rooms_))
+    (do (core/add-room! ?data uid)
+        (core/add-to-room! ?data uid)))
+  (update-clients-rooms uid)
+  (println (str "\nadd room\n" @core/rooms_ "\n" @core/users_ "\n")))
 
 (defmethod -event-msg-handler
   :update/login
   [{:keys [uid ?data]}]
-  (if (login-needed? @users_ uid)
-    (do (add-to-room! rooms_ users_ "public" uid)
-        (set-username! rooms_ users_ uid ?data)
-        (update-clients-rooms @rooms_ @users_ uid)
+  (if (core/login-needed? uid)
+    (do (core/add-to-room! "public" uid)
+        (core/set-username! uid ?data)
+        (update-clients-rooms uid)
         (send-login-need-status uid false)
-        (println (str "\nLOGIN\n" @users_ "\n\n" @rooms_)))))
+        (println (str "\nLOGIN\n" @core/users_ "\n\n" @core/rooms_)))))
 
 ;; ---------- sente router ----------
 
 (defonce router_ (atom nil))
 
 (defn stop-router! []
-  (reset! rooms_ {})
-  (reset! users_ {})
+  (reset! core/rooms_ {})
+  (reset! core/users_ {})
   (when-let [stop-fn @router_] (stop-fn)))
 
 (defn start-router! []
   (stop-router!)
-  (reset! rooms_ (add-room @rooms_ "public"))
-  (add-channel! rooms_ :public "#general2")
-  (add-channel! rooms_ :public "#general3")
+  (reset! core/rooms_ (core/add-room! "public"))
+  (core/add-channel! :public "#general2")
+  (core/add-channel! :public "#general3")
   (reset! router_
           (sente/start-server-chsk-router!
            ch-chsk -event-msg-handler)))
